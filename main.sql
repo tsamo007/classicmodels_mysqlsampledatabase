9694cb7c52c8e0d5828e5f900d490b95e04c3a8b
tsamo007 <107804153+tsamo007@users.noreply.github.com>
2022-07-25 02:22:32 +0100

    Update main.sql

--- main.sql
+++ main.sql
@@ -7,3 +7,18 @@ INNER JOIN offices USING (officeCode)
 group by officeCode
 having Total>1
 ORDER BY Total DESC
+
+# Chiffre daffaire
+
+use classicmodels;
+SELECT year(paymentDate) as Year, sum(amount) as gain
+FROM payments
+GROUP BY year(paymentDate)
+ORDER BY Year;
+
+# Produit le plus vendu 
+SELECT * FROM classicmodels.products;
+SELECT products.productCode, products.productName, count(products.productCode) as sumProduct FROM products
+join orderdetails on products.productCode = orderdetails.productCode
+GROUP BY products.productCode
+ORDER BY count(products.productCode) DESC;
